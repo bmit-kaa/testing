66a9a7bb3de6bbdb7b54f75b6987a6f7918023bf
Melissa Wilkins <melissa.wilkins@espas.ch>
2021-01-06 16:32:58 +0100

    Dateien verschoben HelloButton funktioniert nicht

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import './css/main.css';
+import './js/main.js';
 
 const App = () =>{
     return(
-        <h1>App</h1>
+        <body>
+            <div class="grid-container">
+                <header>
+                    <h1>Wilkommen zum Beacon Projekt</h1>
+                </header>
+                <main>
+                    <button id="helloButton" onclick="displayGreeting()">Hello Beacon</button>
+                    <p id="demo"></p>
+                </main>
+                <footer>
+                    <p>IT Services Espas</p>
+                </footer>
+            </div>
+        </body>
+
     );
 }
 export default App;
