62b2d8f29032661442b0211f798913daeaac06fa
bmit-hul <loris.huetter@espas.ch>
2021-01-27 13:43:23 +0100

    React Beacon get Name and Owner
    Made with react
    
    A button that shows the name and the owners of each Beacon using the Kontakt.io API

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import logo from './espasarrow.png';
+import './App.css';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+
+let htmldeviceid = 0;
+
+var devices = [];
+
+function yeet(){
+  fetch('https://api.kontakt.io/device?', {
+    headers: {
+      'Api-Key': 'nxxuKpSXKLZWEPWZWyeIDblvTsvTYYKA',
+      'Accept': 'application/vnd.com.kontakt+json;version=10'
+    }
+  }).then(response => {
+      response.json().then(r => {
+        devices = r.devices
+        console.log(yeet2())});
+        // setState({devices: devices});
+  })
+}
+
+function yeet2(){
+  // console.log(devices[0].uniqueId);
+  for (let i = 0; i <= 5; i++){
+    htmldeviceid = "beacon" + i.toString(10);
+      const outputone = <div>
+                          <li>This is device Nr{i} with name: {devices[i].uniqueId} and owner: {devices[i].alias}</li>
+                        </div>;
+      ReactDOM.render(
+        outputone,
+        document.getElementById(htmldeviceid)
+      );
+    }
+}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p></p>
+        <button onClick={yeet}>Show Beacons</button>
+        <ul className="beacontext">
+        <a id="beacon0"></a>
+        <a id="beacon1"></a>
+        <a id="beacon2"></a>
+        <a id="beacon3"></a>
+        <a id="beacon4"></a>
+        <a id="beacon5"></a>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
