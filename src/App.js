7d05e0bc352647a603d27048bfaf3461c33e100e
Justin030 <55232645+Justin030@users.noreply.github.com>
2021-04-06 17:59:49 +0200

    feature/extern_api

--- src/App.js
+++ src/App.js
@@ -2,41 +2,9 @@ import logo from './espasarrow.png';
 import './App.css';
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import './api/fetchBeacon'
 
 function App() {
-
-let htmldeviceid = 0;
-
-var devices = [];
-
-function yeet(){
-  fetch('https://api.kontakt.io/device?', {
-    headers: {
-      'Api-Key': 'nxxuKpSXKLZWEPWZWyeIDblvTsvTYYKA',
-      'Accept': 'application/vnd.com.kontakt+json;version=10'
-    }
-  }).then(response => {
-      response.json().then(r => {
-        devices = r.devices
-        console.log(yeet2())});
-        // setState({devices: devices});
-  })
-}
-
-function yeet2(){
-  // console.log(devices[0].uniqueId);
-  for (let i = 0; i <= 5; i++){
-    htmldeviceid = "beacon" + i.toString(10);
-      const outputone = <div>
-                          <li>This is device Nr{i} with name: {devices[i].uniqueId} and owner: {devices[i].alias}</li>
-                        </div>;
-      ReactDOM.render(
-        outputone,
-        document.getElementById(htmldeviceid)
-      );
-    }
-}
-
   return (
    /*  <div className="grid-container">
                 <header>
